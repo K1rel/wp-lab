09dd8b7ddadee1059a4870aaf74237322c4489da
Dandemanor <133015399+K1rel@users.noreply.github.com>
2023-12-20 15:47:20 +0100

    lab1 wp

--- src/main/java/mk/ukim/mk/lab/repository/BookRepository.java
+++ src/main/java/mk/ukim/mk/lab/repository/BookRepository.java
@@ -12,14 +12,15 @@ import java.util.Optional;
 @Repository
 public class BookRepository {
     List<Book> bookList = new ArrayList<>();
-   AuthorRepository authorRepository;
-    public BookRepository() {
-        Book book1 = new Book("1234567890", "Book Title 1", "Fiction", 2020, List.of(authorRepository.findById(1L).orElse(null), authorRepository.findById(2L).orElse(null)));
-        Book book2 = new Book("9876543210", "Book Title 2", "Non-Fiction", 2018, List.of(authorRepository.findById(3L).orElse(null), authorRepository.findById(4L).orElse(null)));
-        Book book3 = new Book("5555555555", "Book Title 3", "Mystery", 2015, List.of(authorRepository.findById(5L).orElse(null)));
-        Book book4 = new Book("1111111111", "Book Title 4", "Science Fiction", 2022, List.of(authorRepository.findById(1L).orElse(null), authorRepository.findById(3L).orElse(null),
-                authorRepository.findById(5L).orElse(null)));
-        Book book5 = new Book("9999999999", "Book Title 5", "Thriller", 2017, List.of(authorRepository.findById(2L).orElse(null), authorRepository.findById(4L).orElse(null)));
+ private final AuthorRepository authorRepository;
+    public BookRepository(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+        Book book1 = new Book("1234567890", "Book Title 1", "Fiction", 2020, List.of(this.authorRepository.findById(1L).orElse(null), this.authorRepository.findById(2L).orElse(null)));
+        Book book2 = new Book("9876543210", "Book Title 2", "Non-Fiction", 2018, List.of(this.authorRepository.findById(3L).orElse(null), this.authorRepository.findById(4L).orElse(null)));
+        Book book3 = new Book("5555555555", "Book Title 3", "Mystery", 2015, List.of(this.authorRepository.findById(5L).orElse(null)));
+        Book book4 = new Book("1111111111", "Book Title 4", "Science Fiction", 2022, List.of(this.authorRepository.findById(1L).orElse(null), this.authorRepository.findById(3L).orElse(null),
+                this.authorRepository.findById(5L).orElse(null)));
+        Book book5 = new Book("9999999999", "Book Title 5", "Thriller", 2017, List.of(this.authorRepository.findById(2L).orElse(null), this.authorRepository.findById(4L).orElse(null)));
     }
 
     public List<Book> findAll(){
--- src/main/java/mk/ukim/mk/lab/web/BookListServlet.java
+++ src/main/java/mk/ukim/mk/lab/web/BookListServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mk.ukim.mk.lab.repository.BookRepository;
 import mk.ukim.mk.lab.service.BookService;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -17,21 +18,23 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/listBooks")
 public class BookListServlet extends HttpServlet {
     private final SpringTemplateEngine springTemplateEngine;
+
     private final BookService bookService;
 
-    public BookListServlet(SpringTemplateEngine springTemplateEngine, BookService bookService) {
+    public BookListServlet(SpringTemplateEngine springTemplateEngine, BookRepository bookRepository, BookService bookService) {
         this.springTemplateEngine = springTemplateEngine;
+
         this.bookService = bookService;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         IWebExchange webExchange = JakartaServletWebApplication
-                .buildApplication(getServletContext())
+                .buildApplication(this.getServletContext())
                 .buildExchange(req, resp);
 
         WebContext context = new WebContext(webExchange);
         context.setVariable("books",bookService.listBooks());
-        springTemplateEngine.process("")
+        springTemplateEngine.process("listBooks.html",context,resp.getWriter());
     }
 }
